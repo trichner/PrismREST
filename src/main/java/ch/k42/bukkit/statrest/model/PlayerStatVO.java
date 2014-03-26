529f31f71847ecdfe7d3fefb21739fd1d75a35e2
Thomas Richner <thomasrichner+github@gmail.com>
2014-03-26 18:30:38 +0100

    Initial commit

---
+++ src/main/java/ch/k42/bukkit/statrest/model/PlayerStatVO.java
@@ -0,0 +1,23 @@
+package ch.k42.bukkit.statrest.model;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ * Created by Thomas on 26.03.14.
+ */
+@XmlRootElement
+public class PlayerStatVO {
+    private String name;
+
+    public PlayerStatVO(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
---
+++ src/main/java/ch/k42/bukkit/statrest/rest/JaxRsActivator.java
@@ -0,0 +1,12 @@
+package ch.k42.bukkit.statrest.rest;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Created by Thomas on 26.03.14.
+ */
+@ApplicationPath("/rest")
+public class JaxRsActivator extends Application {
+    //bootstraps RESTeasy
+}
